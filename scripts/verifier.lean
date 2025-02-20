2505f8407180fefcf5f0bb494a0774417a19258e
riyazahuja <riyaza@andrew.cmu.edu>
2025-02-20 13:34:59 -0500

    update

--- scripts/verifier.lean
+++ scripts/verifier.lean
@@ -2,15 +2,22 @@
 import Cli
 import scripts.state_comments
 
-open Lean Core Elab IO Meta Term Command Tactic
+open Lean Core Elab IO Meta Term Command Tactic Cli
 
 set_option autoImplicit true
 
--- we begin by configuring the modules/theorems we want to improve and the improver settings
--- improver settings are given by annotation, rag, best of n, refinement, etc. and metric config
--- metric config is given as in evaluate/metrics
--- theorems are given by a list of names, and modules are given by a list of names
+structure ImprovedTheoremInstance where
+  originalTheorem : String
+  modelOutput : String
+  stateComments : String
+  correct : Bool
+  metricScore : Nat
+  msgs : List String
 
+structure ImProverConfig where
+  mod : Name
+  decls : Option (List Name) := none
+  jsonPath : Option String := none
 
 def insert_state_comments (step:CompilationStep) (pre_elab_str: Option String := none) : IO String := do
   let mut trees := step.trees
@@ -24,7 +31,7 @@ def insert_state_comments (step:CompilationStep) (pre_elab_str: Option String :=
   /- **TODO**: I changed the logic in runAtDecls below, so now `step.src` is a substring of a different string,
     maybe (all preceding contents ++ this theorem). So the below (might) have to be changed -/
   let mut src := match pre_elab_str with
-                  | none => ({str:=step.src.str, stopPos := step.src.stopPos, startPos := 0} : Substring).toString.splitOn "\n"
+                  | none => step.src.toString.splitOn "\n"
                   | some str => (({str:=step.src.str, stopPos := step.src.startPos, startPos := 0} : Substring).toString ++ str).splitOn "\n"
   let mut inserted : Std.HashSet Nat := Std.HashSet.ofList [10000000]
   for item in L₃.reverse do
@@ -49,95 +56,168 @@ def _root_.Lean.Elab.Command.State.withOptions (state : Command.State) (options 
             opts := opts.insert k v
           opts } }
 
-/-- Our verifier needs two parts: First evaluate a whole module (for each module in config)
-with the proof as sorry option on. this should return a bunch of compilationSteps.
-Then on each decl in this module in the include list, we run the "improvement loop"
 
-This "loop" for will take in the now will literally just print out the
-theorem with the proof states interleaved.
+def promptModel (cmd : CompilationStep) (model : String := "nutPace/Improver-DeepSeek-R1-Distill-Qwen-7B_full")
+  (endpoint: String := "http://0.0.0.0:8000/v1/chat/completions") (best_of_n : Nat := 1) : IO (List String) := do
+  let srcCommand := cmd.src.toString
+  -- IO.println s!"srcCommand:\n{srcCommand.dropRightWhile (· == '\n')}"
 
-Then we will elaborate this string on the environment before the command which created that declaration.
-then we will print out the theorem with the proof states interleaved.
---/
+  let prompt : String := s!"Shorten the current theorem (wrapped in <CURRENT>...</CURRENT>) to be as short as possible in length - measured in the number of tactics in the proof - while also ensuring that the output is still a correct proof of the theorem. Include the output in the <IMPROVED>...</IMPROVED> tag.\n\n<CURRENT>\n{srcCommand}\n</CURRENT>\n\n<IMPROVED>"
+  let jsonPayload : Json := Json.mkObj [
+      ("model", Json.str model),
+      ("messages", Json.arr #[Json.mkObj [("role",Json.str "user"),("content", Json.str prompt)]]),
+      ("max_tokens", Json.num <| JsonNumber.fromNat 4096)
+    ]
+  let args := #[
+    "-X", "POST",
+    "-H", "Content-Type: application/json",
+    "-d", s!"{jsonPayload.compress}",
+    endpoint
+  ]
 
-def runAtDecls (mod : Name) (decls : Option (List Name) := none): IO Unit := do
+  let tasks := List.range (best_of_n) |>.map fun _ => IO.asTask (prio := Task.Priority.dedicated) do
+
+    let out_json ← IO.Process.output {
+      cmd := "curl",
+      args := args
+    }
+    return out_json
+
+  let newCommandCandidates ← tasks.mapM fun (t : BaseIO _) => do
+    IO.ofExcept <| (← t).get
+
+
+  let newCommandCandidates ← newCommandCandidates.mapM (fun out_json => do
+    let out_json_parsed : Json := (match Json.parse out_json.stdout with
+                          | Except.error _ => none
+                          | Except.ok msg => some msg).get!
+
+    let out := match out_json_parsed with
+              | Json.obj kvs => match kvs.find compare "choices" with
+                | some (Json.arr choices) => match choices[0]? with
+                  | some (Json.obj choice) => match choice.find compare "message" with
+                    | some (Json.obj message) => match message.find compare "content" with
+                      | some (Json.str s) => some s
+                      | _ => none
+                    | _ =>none
+                  | _ => none
+                | _ => none
+              | _ => none
+
+    let modelOutput := out.get!
+
+    let tagOpen  := "<IMPROVED>"
+    let tagClose := "</IMPROVED>"
+    let trimmed_out := modelOutput.stripPrefix tagOpen |>.stripSuffix tagClose
+
+    return trimmed_out)
+
+  return newCommandCandidates
+
+
+def promptModel_debug (cmd : CompilationStep) : IO (List String) := do
+  let srcCommand := cmd.src.toString
+  return ["--DEBUG\n"++srcCommand]
+
+
+def elaborateVariants (original : CompilationStep) (mod: Name) (variants : List String) : IO (List (Option (String × CompilationStep))) := do
+  let options := ({} : KVMap)
+      |>.insert `maxHeartbeats (.ofNat 200000) -- TODO determine a heartbeat count
+      |>.insert `debug.proofAsSorry (.ofBool false) -- turn proof checking back on
+
   let fileName := (← findLean mod).toString
 
+  let contentsBefore : Substring := match original.src with
+    | ⟨s, b, _⟩ => ⟨s, 0, b⟩
+
+  let tasks := variants.map fun newCommand => IO.asTask (prio := Task.Priority.dedicated) do
+    let elaborated_steps := Lean.Elab.IO.compilationSteps
+      (Parser.mkInputContext (contentsBefore.toString ++ newCommand) fileName)
+      original.parserStateBefore
+      (original.commandStateBefore.withOptions options)
+
+    let head? ← elaborated_steps.uncons
+    return match head? with
+      | none => none
+      | some (head, _) => some (newCommand,head)
+
+  let results ← tasks.mapM fun (t : BaseIO _) => do
+    IO.ofExcept <| (← t).get
+  return results
+
+
+def ImProver (config : ImProverConfig): IO Unit := do
+  let ⟨mod, decls, json_path⟩ := config
+  let fileName := (← findLean mod).toString
+  let mut trajectories_json := []
   /- TODO: I don't know if proofAsSorry is actually working -/
   let proofAsSorry := ({} : KVMap).insert `debug.proofAsSorry (.ofBool true)
   let steps := Lean.Elab.IO.processInput' (← moduleSource mod) none proofAsSorry fileName
 
   let targets := steps.bind fun c => (MLList.ofList c.diff).map fun i => (c, i)
-
   for (cmd, ci) in targets do
     if decls.isSome && !(decls.get!.contains ci.name) then
       continue
+    IO.println s!"============================================="
+    IO.println s!"Processing {ci.name} in {mod}"
+    IO.println s!"---------------------------------------------"
 
     for m in cmd.msgs do IO.eprintln (bombEmoji ++ (← m.data.toString))
     unless cmd.msgs.isEmpty do
       throw <| IO.userError s!"Unexpected messages in: {mod} during elaboration of {cmd.stx}"
 
-    let contentsBefore : Substring := match cmd.src with
-      | ⟨s, b, _⟩ => ⟨s, 0, b⟩
-    let srcCommand := cmd.src.toString
-    IO.println s!"COMPILATION STEP CONTENTS:\n{srcCommand.dropRightWhile (· == '\n')}"
+    -- let newCommandCandidates ← promptModel cmd (best_of_n := 5)
+    let newCommandCandidates ← promptModel_debug cmd
+    let resultantSteps := (← elaborateVariants cmd mod newCommandCandidates).filterMap (fun x => x)
 
-    /- Presumably, interaction with the LLM improver agent happens here.
-      Given e.g. the srcCommand (source theorem before improvement),
-      or e.g. ← insert_state_comments cmd (source theorem before improvement + state comments),
-      the LLM outputs its proof improvement candidates to newCommandCandidates.
-      Here we (1) don't do any changes and (2) replace rfl by sorry as a toy example. -/
-    let newCommandCandidates := [
-      srcCommand,
-      srcCommand.replace "rfl" "sorry"
-    ]
 
-    let options := ({} : KVMap)
-      |>.insert `maxHeartbeats (.ofNat 200000) -- TODO determine a heartbeat count
-      |>.insert `debug.proofAsSorry (.ofBool false) -- turn proof checking back on
 
-    /- Run proof candidates in parallel -/
-    let tasks := newCommandCandidates.map fun newCommand => IO.asTask do
-      let mut msgs := #[]
-      let mut correct := false
-      let elaborated_steps := Lean.Elab.IO.compilationSteps
-        (Parser.mkInputContext (contentsBefore.toString ++ newCommand) fileName)
-        cmd.parserStateBefore
-        (cmd.commandStateBefore.withOptions options)
 
-      let head? ← elaborated_steps.uncons
-      match head? with
-      | none =>
-        msgs := msgs.push s!"No elaborated steps"
-      | some (head, _) =>
-        -- Should probably check that ci is actually in the diff? But the below code is a bit finnicky with namespaces.
-        -- works fine without it anyways
+    let instances : List ImprovedTheoremInstance ← resultantSteps.mapM (fun (model_output,head) => do
+      let correct := head.msgs.isEmpty
+      let metric_score := head.trees.flatMap (fun tree=>tree.tactics) |>.length
+      let state_comments ← insert_state_comments head
 
-        -- if not (head.after.constants.map₂.contains ci.name) then
-        --   IO.eprintln s!"Expected {ci.name} to be in the elaborated steps, but it was not:\n {(head.diff.map (fun info=>info.name))}"
-        -- else
-        msgs := msgs.push s!"NEW COMMAND:\n{newCommand}"
-        msgs := msgs.push s!"CONSTANTS:\n{head.before.constants.map₂.toList.map (fun (x,v)=>x)}"
-        msgs := msgs.push s!"CONSTANTS:\n{head.after.constants.map₂.toList.map (fun (x,v)=>x)}"
-        msgs := msgs.push s!"AFTER ELAB CONTENTS:\n {← insert_state_comments head}"
+      let msgs ← head.msgs.mapM (fun msg => do
+        let m ← msg.data.toString
+        return bombEmoji++m)
 
-        /- Any errors in the improved proof will be caught here -/
-        for m in head.msgs do
-          msgs := msgs.push (bombEmoji ++ (← m.data.toString))
-        correct := head.msgs.isEmpty
+      return ⟨cmd.src.toString, model_output, state_comments, correct, metric_score, msgs⟩
+    )
 
-      return (correct, msgs)
 
-    let results ← tasks.mapM fun (t : BaseIO _) => do
-      IO.ofExcept <| (← t).get
-    for result in results do
-      IO.println "============================================="
-      let (correct, msgs) := result
+    for i in instances do
+      IO.println "-------------------------------------------------"
+      let ⟨original, newCommand,elabed, correct, metric, msgs⟩ := i
+      IO.println s!"Original:\n {original}"
+      IO.println s!"Model Output:\n {newCommand}"
+      IO.println s!"Compiled:\n {elabed}"
       IO.println s!"Correct: {correct}"
+      IO.println s!"Metric: {metric}"
       for msg in msgs do
         IO.println msg
+      IO.println "-------------------------------------------------"
+
+    let trajectories_json_new := instances.map (fun i =>
+      let ⟨original,newCmd,elabed, correct,metric,msgs⟩ := i
+      Json.mkObj [
+        ("module", Json.str mod.toString),
+        ("original", original),
+        ("new", Json.str newCmd),
+        ("annotated",Json.str elabed),
+        ("correct",Json.bool correct),
+        ("metric",Json.num <| JsonNumber.fromNat metric),
+        ("errors", Json.str ("\n\n".intercalate msgs))
+        ])
+    trajectories_json := trajectories_json ++ trajectories_json_new
+
+  let trajectories := Json.arr (trajectories_json.toArray)
+  match json_path with
+  | some path =>
+    IO.FS.writeFile path (trajectories.compress)
+  | none => pure ()
 
 
-#eval runAtDecls `temp.temp
 
-#eval runAtDecls `Mathlib.Logic.Hydra -- (some [`Relation.cutExpand_le_invImage_lex])
+
+#eval ImProver {mod:=`temp.temp, decls:= some [`theorem1]}
