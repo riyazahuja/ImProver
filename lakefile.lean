9c13d2771f486ddedbda254db4a14440ee3def45
riyazahuja <riyaza@andrew.cmu.edu>
2025-02-26 19:45:54 -0500

    mixed

--- lakefile.lean
+++ lakefile.lean
@@ -5,9 +5,11 @@ package «lean-training-data» {
   -- add any package configuration options here
 }
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "e9ae2a61ef5c99d6edac84f0d04f6324c5d97f67"
+-- require mathlib from git
+--   "https://github.com/leanprover-community/mathlib4.git" @ "e9ae2a61ef5c99d6edac84f0d04f6324c5d97f67"
 
+require mil from git
+  "https://github.com/riyazahuja/Improver_MIL" @ "new"
 
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "v4.15.0"
 
