0a499c256f6654acc2f3a175864c7bd143671372
riyazahuja <riyaza@andrew.cmu.edu>
2025-03-16 00:15:03 -0400

    p

--- RAG/RAG_mathlib.py
+++ RAG/RAG_mathlib.py
@@ -21,49 +21,67 @@ ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Processed with the ntp-toolkit repository
 def annotated_thms_generator_file(
-        path_to_ntp_toolkit=os.path.join(
-            os.path.abspath(os.path.join(ROOT_PATH, os.pardir)),
-                                           "ntp-toolkit",
-                                           "Examples",
-                                           "mathlib",
-                                           "StateComments")):
+    path_to_ntp_toolkit=os.path.join(
+        os.path.abspath(os.path.join(ROOT_PATH, os.pardir)),
+        "ntp-toolkit",
+        "Examples",
+        "mathlib",
+        "StateComments",
+    )
+):
     for file in os.listdir(path_to_ntp_toolkit):
         if file.endswith(".lean"):
             with open(os.path.join(path_to_ntp_toolkit, file), "r") as f:
                 yield file, [f.read()]
 
-def get_library_lean_files(library_name="Mathlib",
-                           path=os.path.join(ROOT_PATH,
-                                             ".lake",
-                                             "packages",
-                                             "mathlib",
-                                             "Mathlib")):
-    cmd = f"find {path} -type f -name \"*.lean\" -print"
-    files = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True).stdout.split("\n")
+
+def get_library_lean_files(
+    library_name="Mathlib",
+    path=os.path.join(ROOT_PATH, ".lake", "packages", "mathlib", "Mathlib"),
+):
+    cmd = f'find {path} -type f -name "*.lean" -print'
+    files = subprocess.run(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True
+    ).stdout.split("\n")
     for i in range(len(files)):
         try:
-            files[i] = (library_name + files[i].split(library_name)[1]).replace("/", ".").split(".lean")[0]
+            files[i] = (
+                (library_name + files[i].split(library_name)[1])
+                .replace("/", ".")
+                .split(".lean")[0]
+            )
         except IndexError:
             files[i] = ""
     return list(filter(None, files))
 
-def save_annotated_library(library_name="Mathlib",
-                           path=os.path.join(ROOT_PATH,
-                                             ".lake",
-                                             "packages",
-                                             "mathlib",
-                                             "Mathlib")):
+
+def save_annotated_library(
+    library_name="Mathlib",
+    path=os.path.join(ROOT_PATH, ".lake", "packages", "mathlib", "Mathlib"),
+):
     modules = get_library_lean_files(library_name, path)
     # modules = ['Mathlib.Tactic.LinearCombination.Lemmas', 'Mathlib.Tactic.ToAdditive', 'Mathlib.Tactic.Algebraize', 'Mathlib.Tactic.CancelDenoms.Core', 'Mathlib.Tactic.Continuity.Init', 'Mathlib.Tactic.Check', 'Mathlib.Tactic.Generalize', 'Mathlib.Tactic.ExtractGoal', 'Mathlib.Tactic.SuccessIfFailWithMsg', 'Mathlib.Tactic.Clear_']
     if not os.path.exists(os.path.join(ROOT_PATH, "RAG", "annotated", library_name)):
         os.makedirs(os.path.join(ROOT_PATH, "RAG", "annotated", library_name))
+
     def annotateModule(module):
         cmd = ["lake", "exe", "StateComments", module]
-        out = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True, universal_newlines=True, cwd=ROOT_PATH)
-        with open(os.path.join(ROOT_PATH, "RAG", "annotated", library_name, f"{module}.lean"), "w") as f:
+        out = subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            text=True,
+            universal_newlines=True,
+            cwd=ROOT_PATH,
+        )
+        with open(
+            os.path.join(ROOT_PATH, "RAG", "annotated", library_name, f"{module}.lean"),
+            "w",
+        ) as f:
             f.write(out.stdout)
         print(f"Annotated {module}")
         return out.stdout
+
     futures = []
     with ThreadPoolExecutor() as executor:
         for module in modules:
@@ -71,12 +89,17 @@ def save_annotated_library(library_name="Mathlib",
     # for future in futures:
     #     print(future.result())
 
+
 def create_database_of_annotated(replace=False, max_docs=None, package_name="Mathlib"):
     path_to_annotated = os.path.join(ROOT_PATH, "RAG", "annotated", package_name)
     if not os.path.exists(path_to_annotated):
-        print(f"No annotated theorems found at {path_to_annotated}. Run save_annotated_library() to generate them.")
+        print(
+            f"No annotated theorems found at {path_to_annotated}. Run save_annotated_library() to generate them."
+        )
 
-    database_path = os.path.join(ROOT_PATH, ".db", f"{package_name.lower()}_annotated_db")
+    database_path = os.path.join(
+        ROOT_PATH, ".db", f"{package_name.lower()}_annotated_db"
+    )
 
     if replace:
         if os.path.exists(database_path):
@@ -112,27 +135,40 @@ def create_database_of_annotated(replace=False, max_docs=None, package_name="Mat
     vectorstore = Chroma(
         collection_name="Annotated_Mathlib_Theorems",
         persist_directory=database_path,
-        embedding_function=embeddings)
+        embedding_function=embeddings,
+    )
 
     def embed(doc):
         return embeddings.embed_query(doc.page_content)
 
     docs = docs[:max_docs] if max_docs is not None else docs
+    docs = docs[:100]
     # vectorstore.add_documents(docs)
     with ProcessPoolExecutor() as executor:
         emb = executor.map(embed, docs)
     vectorstore.add_documents(docs, embeddings=emb)
     return vectorstore
 
-def get_database_retriever(package_name="Mathlib",number_to_retrieve=6, filter={}):
-    database_path = os.path.join(ROOT_PATH, ".db", f"{package_name.lower()}_annotated_db")
-    embeddings = OllamaEmbeddings(model="llama3.2")
-    database = Chroma(collection_name=f"Annotated_Theorems_{package_name}",
-                      persist_directory=database_path,
-                      embedding_function=embeddings)
-    return database.as_retriever(
-        search_type="mmr", search_kwargs={"k": number_to_retrieve, "filter": filter}
+
+def get_database_retriever(package_name="Mathlib", number_to_retrieve=6, filter={}):
+    database_path = os.path.join(
+        ROOT_PATH, ".db", f"{package_name.lower()}_annotated_db"
     )
+    embeddings = OllamaEmbeddings(model="llama3.2")
+    # database = Chroma(
+    #     collection_name=f"Annotated_{package_name}_Theorems",
+    #     # persist_directory=database_path,
+    #     embedding_function=embeddings,
+    # )
+    database = Chroma(
+        collection_name="Annotated_Mathlib_Theorems",
+        persist_directory=database_path,
+        embedding_function=embeddings,
+    )
+    return database.as_retriever(
+        search_type="mmr", search_kwargs={"k": number_to_retrieve}
+    )
+
 
 def test_average_speed():
     import time
@@ -141,23 +177,38 @@ def test_average_speed():
     get_database_retriever(replace=True, max_docs=1000)
     print(f"Time per chunk: {(time.time() - start) / 1000}")
 
+
 def annotate_all_packages(project_home=ROOT_PATH):
     for package_dir in os.listdir(os.path.join(project_home, ".lake", "packages")):
-        if os.path.isdir(os.path.join(ROOT_PATH, ".lake", "packages", package_dir, package_dir.title())):
-            save_annotated_library(library_name=package_dir,
-                                   path=os.path.join(ROOT_PATH, ".lake", "packages", package_dir))
+        if os.path.isdir(
+            os.path.join(
+                ROOT_PATH, ".lake", "packages", package_dir, package_dir.title()
+            )
+        ):
+            save_annotated_library(
+                library_name=package_dir,
+                path=os.path.join(ROOT_PATH, ".lake", "packages", package_dir),
+            )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Annotate and save theorems
     # save_annotated_library()
 
     # Compile the database (takes a hot minute)
-    create_database_of_annotated(replace=True)
+    # create_database_of_annotated(replace=True)
 
     # Test retrieval
-    # retriever = get_database_retriever()
-    # output = retriever.invoke("""elab "generalize'" h:ident " : " t:term:51 " = " x:ident : tactic => do""")
-    # for doc in output:
-    #     print(f"[{doc.metadata}]")
-    #     print(doc.page_content)
-    #     print("===============")
+    retriever = get_database_retriever()
+    output = retriever.invoke(
+        """variable (m) in
+/-- Delete the leading term in a multivariate polynomial (for some monomial order) -/
+noncomputable def subLTerm (f : MvPolynomial σ R) : MvPolynomial σ R :=
+  f - monomial (m.degree f) (m.lCoeff f)"""
+    )
+    print(output)
+    print(type(output))
+    for doc in output:
+        print(f"[{doc.metadata}]")
+        print(doc.page_content)
+        print("===============")
