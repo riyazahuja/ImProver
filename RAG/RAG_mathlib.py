880a2e68ad64a25da235da7c6cf64d25a29a0022
taterowney <51540870+taterowney@users.noreply.github.com>
2025-03-17 22:43:14 -0400

    Added better RAG method

--- RAG/RAG_mathlib.py
+++ RAG/RAG_mathlib.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from langchain.globals import set_debug
+# from ollama import embeddings
 
 set_debug(False)
 
@@ -21,20 +22,20 @@ ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # DB_PATH = os.path.join(ROOT_PATH, ".db", ".mathlib_annotated_db")
 
 
-# Processed with the ntp-toolkit repository
-def annotated_thms_generator_file(
-    path_to_ntp_toolkit=os.path.join(
-        os.path.abspath(os.path.join(ROOT_PATH, os.pardir)),
-        "ntp-toolkit",
-        "Examples",
-        "mathlib",
-        "StateComments",
-    )
-):
-    for file in os.listdir(path_to_ntp_toolkit):
-        if file.endswith(".lean"):
-            with open(os.path.join(path_to_ntp_toolkit, file), "r") as f:
-                yield file, [f.read()]
+# Processed with the ntp-toolkit repository (Deprecated)
+# def annotated_thms_generator_file(
+#     path_to_ntp_toolkit=os.path.join(
+#         os.path.abspath(os.path.join(ROOT_PATH, os.pardir)),
+#         "ntp-toolkit",
+#         "Examples",
+#         "mathlib",
+#         "StateComments",
+#     )
+# ):
+#     for file in os.listdir(path_to_ntp_toolkit):
+#         if file.endswith(".lean"):
+#             with open(os.path.join(path_to_ntp_toolkit, file), "r") as f:
+#                 yield file, [f.read()]
 
 
 def get_library_lean_files(
@@ -60,29 +61,53 @@ def get_library_lean_files(
 def save_annotated_library(
     library_name="Mathlib",
     path=os.path.join(ROOT_PATH, ".lake", "packages", "mathlib", "Mathlib"),
+    style="new"
 ):
     modules = get_library_lean_files(library_name, path)
-    # modules = ['Mathlib.Tactic.LinearCombination.Lemmas', 'Mathlib.Tactic.ToAdditive', 'Mathlib.Tactic.Algebraize', 'Mathlib.Tactic.CancelDenoms.Core', 'Mathlib.Tactic.Continuity.Init', 'Mathlib.Tactic.Check', 'Mathlib.Tactic.Generalize', 'Mathlib.Tactic.ExtractGoal', 'Mathlib.Tactic.SuccessIfFailWithMsg', 'Mathlib.Tactic.Clear_']
-    if not os.path.exists(os.path.join(ROOT_PATH, "RAG", "annotated", library_name)):
-        os.makedirs(os.path.join(ROOT_PATH, "RAG", "annotated", library_name))
+    # modules = ['Mathlib.Data.Finset.MulAntidiagonal', 'Mathlib.Data.Complex.Cardinality', 'Mathlib.Topology.Category.TopCat.EffectiveEpi']
+    if style == "new":
+        if not os.path.exists(os.path.join(ROOT_PATH, "RAG", "annotated_new", library_name)):
+            os.makedirs(os.path.join(ROOT_PATH, "RAG", "annotated_new", library_name))
+    else:
+        if not os.path.exists(os.path.join(ROOT_PATH, "RAG", "annotated", library_name)):
+            os.makedirs(os.path.join(ROOT_PATH, "RAG", "annotated", library_name))
 
     def annotateModule(module):
-        cmd = ["lake", "exe", "StateComments", module]
-        out = subprocess.run(
-            cmd,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL,
-            text=True,
-            universal_newlines=True,
-            cwd=ROOT_PATH,
-        )
-        with open(
-            os.path.join(ROOT_PATH, "RAG", "annotated", library_name, f"{module}.lean"),
-            "w",
-        ) as f:
-            f.write(out.stdout)
-        print(f"Annotated {module}")
-        return out.stdout
+        if style == "new":
+            cmd = ["lake", "exe", "extract_states", module]
+            out = subprocess.run(
+                cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.DEVNULL,
+                text=True,
+                universal_newlines=True,
+                cwd=ROOT_PATH,
+            )
+            if out.stdout:
+                with open(
+                    os.path.join(ROOT_PATH, "RAG", "annotated_new", library_name, f"{module}.jsonl"),
+                    "w",
+                ) as f:
+                    f.write(out.stdout)
+                print(f"Annotated {module}")
+            return out.stdout
+        else:
+            cmd = ["lake", "exe", "StateComments", module]
+            out = subprocess.run(
+                cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.DEVNULL,
+                text=True,
+                universal_newlines=True,
+                cwd=ROOT_PATH,
+            )
+            with open(
+                os.path.join(ROOT_PATH, "RAG", "annotated", library_name, f"{module}.lean"),
+                "w",
+            ) as f:
+                f.write(out.stdout)
+            print(f"Annotated {module}")
+            return out.stdout
 
     futures = []
     with ThreadPoolExecutor() as executor:
@@ -134,10 +159,10 @@ def create_database_of_annotated(replace=False, max_docs=None, package_name="Mat
     docs = splitter.split_documents(docs)
     print("Number of chunks:", len(docs))
     embeddings = OllamaEmbeddings(model="llama3.2")
-
     # embeddings = HuggingFaceEmbeddings(
     #     model_name="riyazahuja/Improver-DeepSeek-R1-Distill-Qwen-7B_full_4096"
     # )
+
 
     vectorstore = Chroma(
         collection_name="Annotated_Mathlib_Theorems",
@@ -146,14 +171,69 @@ def create_database_of_annotated(replace=False, max_docs=None, package_name="Mat
     )
 
     docs = docs[:max_docs] if max_docs is not None else docs
-    docs = docs[:100]
+    # docs = docs[:100]
     # vectorstore.add_documents(docs)
 
     vectorstore.add_documents(docs)
     return vectorstore
 
 
-def get_database_retriever(package_name="Mathlib", number_to_retrieve=6, filter={}):
+
+def create_database_initial_proofstate(replace=False, max_docs=None, package_name="Mathlib"):
+    path_to_annotated = os.path.join(ROOT_PATH, "RAG", "annotated_new", package_name)
+    if not os.path.exists(path_to_annotated):
+        print(
+            f"No annotated theorems found at {path_to_annotated}. Run save_annotated_library() to generate them."
+        )
+
+    database_path = os.path.join(
+        ROOT_PATH, ".db", f"{package_name.lower()}_initial_proofstate_db"
+    )
+
+    if replace:
+        if os.path.exists(database_path):
+            shutil.rmtree(database_path)
+
+    # loader = DirectoryLoader(
+    #     path_to_annotated, glob="**/*.lean", show_progress=True, loader_cls=TextLoader
+    # )
+
+    # docs = loader.load()
+
+    docs = []
+    for file in os.listdir(path_to_annotated):
+        if file.endswith(".jsonl"):
+            with open(os.path.join(path_to_annotated, file), "r") as f:
+                for line in f:
+                    j = json.loads(line)
+                    # print(j["initialProofState"][:1000])
+                    # print("\n\n----------------\n\n")
+                    docs.append(Document(page_content=j["initialProofState"], metadata={"decl" : j["decl"]}))
+
+    # embeddings = OllamaEmbeddings(model="llama3.2")
+    # embeddings = HuggingFaceEmbeddings(
+    #     model_name="riyazahuja/Improver-DeepSeek-R1-Distill-Qwen-7B_full_4096"
+    # )
+    embeddings = HuggingFaceEmbeddings(
+        model_name="hanwenzhu/all-distilroberta-v1-lr2e-4-bs256-nneg3-ml-mar13"
+    )
+
+    vectorstore = Chroma(
+        collection_name="Mathlib_initial_proofstate_db",
+        persist_directory=database_path,
+        embedding_function=embeddings,
+    )
+
+    docs = docs[:max_docs] if max_docs is not None else docs
+    # docs = docs[:100]
+    # vectorstore.add_documents(docs)
+
+    vectorstore.add_documents(docs)
+    return vectorstore
+
+
+
+def get_database_retriever_old(package_name="Mathlib", number_to_retrieve=6, filter={}):
     database_path = os.path.join(
         ROOT_PATH, ".db", f"{package_name.lower()}_annotated_db2"
     )
@@ -161,6 +241,10 @@ def get_database_retriever(package_name="Mathlib", number_to_retrieve=6, filter=
     #     model_name="riyazahuja/Improver-DeepSeek-R1-Distill-Qwen-7B_full_4096"
     # )
     embeddings = OllamaEmbeddings(model="llama3.2")
+    # embeddings = HuggingFaceEmbeddings(
+    #     model_name="hanwenzhu/all-distilroberta-v1-lr2e-4-bs256-nneg3-ml-mar13"
+    # )
+
     # database = Chroma(
     #     collection_name=f"Annotated_{package_name}_Theorems",
     #     # persist_directory=database_path,
@@ -174,6 +258,40 @@ def get_database_retriever(package_name="Mathlib", number_to_retrieve=6, filter=
     return database.as_retriever(
         search_type="mmr", search_kwargs={"k": number_to_retrieve}
     )
+
+def get_database_retriever(package_name="Mathlib", number_to_retrieve=6, filter={}):
+    database_path = os.path.join(
+        ROOT_PATH, ".db", f"{package_name.lower()}_initial_proofstate_db"
+    )
+    # embeddings = HuggingFaceEmbeddings(
+    #     model_name="riyazahuja/Improver-DeepSeek-R1-Distill-Qwen-7B_full_4096"
+    # )
+    # embeddings = OllamaEmbeddings(model="llama3.2")
+    embeddings = HuggingFaceEmbeddings(
+        model_name="hanwenzhu/all-distilroberta-v1-lr2e-4-bs256-nneg3-ml-mar13"
+    )
+
+    # database = Chroma(
+    #     collection_name=f"Annotated_{package_name}_Theorems",
+    #     # persist_directory=database_path,
+    #     embedding_function=embeddings,
+    # )
+    database = Chroma(
+        collection_name="Mathlib_initial_proofstate_db",
+        persist_directory=database_path,
+        embedding_function=embeddings,
+    )
+    db = database.as_retriever(
+        search_type="mmr", search_kwargs={"k": number_to_retrieve}
+    )
+
+    return db
+    # def get(initialStateString):
+    #     # return db.invoke(initialStateString, filter=filter).metadata["decl"]
+    #     docs = database.max_marginal_relevance_search(initialStateString, k=number_to_retrieve, fetch_k=2*number_to_retrieve, filter=filter)
+    #     # print(docs)
+    #     return [doc.metadata["decl"] for doc in docs]
+    # return get
 
 
 def test_average_speed():
@@ -202,15 +320,24 @@ if __name__ == "__main__":
     # save_annotated_library()
 
     # Compile the database (takes a hot minute)
-    create_database_of_annotated(replace=True)
+    create_database_initial_proofstate(replace=True)
+
+    # Old version
+    # create_database_of_annotated(replace=True)
+
 
     # Test retrieval
+    # IMPORTANT: for new version of retrieval, make sure to run the "getInitialProofState" function in scripts.extract_states, and use that as your query
+    # (it will still return the normal text of the theorem)
 #     retriever = get_database_retriever()
 #     output = retriever.invoke(
-#         """variable (m) in
-# /-- Delete the leading term in a multivariate polynomial (for some monomial order) -/
-# noncomputable def subLTerm (f : MvPolynomial σ R) : MvPolynomial σ R :=
-#   f - monomial (m.degree f) (m.lCoeff f)"""
+#         """α : Type u_1
+# E : Type u_2
+# inst✝ : NormedField E
+# f : α → E
+# hf : Multipliable f
+# ⊢ Eq (Norm.norm (tprod fun i => f i)) (tprod fun i => Norm.norm (f i))
+# """
 #     )
 #     print(output)
 #     print(type(output))
