36ed3c6411f1cb5d038ee0922199139bd0bf8fa2
riyazahuja <riyaza@andrew.cmu.edu>
2025-06-03 15:04:14 -0400

    cleanup again

--- ImProver/efficient/batch_infer.sh
+++ ImProver/efficient/batch_infer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=infer_iter0-q
-#SBATCH --output=logs/infer_iter0_lenq.out
-#SBATCH --error=logs/infer_iter0_lenq.err
+#SBATCH --job-name=infer_iter0-14
+#SBATCH --output=logs/infer_iter0_len14.out
+#SBATCH --error=logs/infer_iter0_len14.err
 #SBATCH --cpus-per-task=12
 #SBATCH --time=1-00:00:00
 #SBATCH --gres=gpu:A6000:6
@@ -17,4 +17,4 @@ export HF_HOME="/data/user_data/riyaza/HF"
 
 cd ~/eval_improver/improver
 
-python /home/riyaza/eval_improver/improver/ImProver/efficient/inference.py length /home/riyaza/eval_improver/improver/scripts/data/tt_split_data_iter0.json --cpus 12 --gpus 6 --examples 2 --n 32 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
+python /home/riyaza/eval_improver/improver/ImProver/efficient/inference.py length /home/riyaza/eval_improver/improver/scripts/data/tt_split_data_iter0.json --cpus 12 --gpus 6 --examples 2 --n 32 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-14B
